dee2f398496b6972c5002fe5ea7ef74e111331f4
Matt Chandler <mkchandler@gmail.com>
2013-12-17 19:19:25 -0600

    Updated the web api project

--- src/BolfTracker.Web.Api/Areas/HelpPage/Controllers/HelpController.cs
+++ src/BolfTracker.Web.Api/Areas/HelpPage/Controllers/HelpController.cs
@@ -24,6 +24,7 @@ namespace BolfTracker.Web.Api.Areas.HelpPage.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.DocumentationProvider = Configuration.Services.GetDocumentationProvider();
             return View(Configuration.Services.GetApiExplorer().ApiDescriptions);
         }
 
